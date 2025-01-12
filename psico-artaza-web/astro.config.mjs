b12cf783bec7b0b759ef0da7def1c46bce91d207
emir-argcoder <ea.samsam@gmail.com>
2025-01-12 19:56:06 -0300

    Netlify changes.

--- psico-artaza-web/astro.config.mjs
+++ psico-artaza-web/astro.config.mjs
@@ -1,14 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
-
 import netlify from '@astrojs/netlify';
-
 import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
   output: 'server',
-  adapter: netlify()
+  adapter: netlify({
+    assets: false  // Deshabilitar soporte experimental para "assets"
+  })
 });
